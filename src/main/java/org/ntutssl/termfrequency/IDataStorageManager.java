70753db19573228fe339275bac7ec0fde6e5e7a0
fantong <fankaihsiang11@gmail.com>
2021-03-17 19:20:27 +0800

    init hw3

--- src/main/java/org/ntutssl/termfrequency/IDataStorageManager.java
+++
@@ -1,7 +0,0 @@
-package org.ntutssl.termfrequency;
-
-import java.util.List;
-
-public interface IDataStorageManager {
-    public List<String> getWords();
-}
--- src/main/java/org/ntutssl/termfrequency/IStopWordManager.java
+++
@@ -1,7 +0,0 @@
-package org.ntutssl.termfrequency;
-
-public interface IStopWordManager {
-    public boolean isStopWordList(String word);
-
-    public boolean isStopWordSet(String word);
-}
