998c68fed96835113f80f6442260c63ad4412caf
fantong <fankaihsiang11@gmail.com>
2021-03-18 16:16:42 +0800

    init hw3

--- src/main/java/org/ntutssl/document/Document.java
+++ src/main/java/org/ntutssl/document/Document.java
@@ -1,3 +1,9 @@
 package org.ntutssl.document;
 
-public interface Document {  }
+public interface Document {
+    public Document getContent();
+
+    public int getLevel();
+
+    public void add(Document document);
+}
