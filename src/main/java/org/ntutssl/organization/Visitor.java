234f5ee7c18839290136bc172cea823c4d8f2ebf
fantong <fankaihsiang11@gmail.com>
2021-05-06 08:06:57 +0800

    hw5

--- src/main/java/org/ntutssl/organization/Visitor.java
+++
@@ -1,9 +0,0 @@
-package org.ntutssl.organization;
-
-public interface Visitor<T> {
-  public void visitTeam(Team team);
-
-  public void visitIndividual(Individual individual);
-
-  public T getResult();
-}
