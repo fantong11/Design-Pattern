f904cee5d434f620ffa77c212aa7082ae256b333
107590051 <t107590051@ntut.org.tw>
2021-06-13 20:48:00 +0800

    hw8

--- src/main/java/org/ntutssl/shop/PayByCreditCardStrategy.java
+++ src/main/java/org/ntutssl/shop/PayByCreditCardStrategy.java
@@ -21,45 +21,48 @@ public class PayByCreditCardStrategy implements PayStrategy, EventListener {
 	 */
 	@Override
 	public void calculate(double totalPrice) {
+		// Scanner scanner = new Scanner(System.in);
+		// System.out.print("Enter the card number: ");
+		// String cardNumber = scanner.nextLine();
+
+		// System.out.print("Eneter the card expiration date 'mm/yy': ");
+		// String date = scanner.nextLine();
+
+		// System.out.print("Enter the CVV code:");
+		// String CVV = scanner.nextLine();
+
+		// if (!(cardNumber.matches("\\d{16}") && date.matches("0[1-9]|1[0-2]/\\d{2}")
+		// && CVV.matches("\\d{3}"))) {
+		// System.out.print("Pay failed.\n");
+		// return;
+		// }
+
+		// DecimalFormat format = new DecimalFormat("###.00");
+		// String resultPrice = "$" + format.format((totalPrice - (totalPrice * 0.1)));
+		// EventManager.getInstance().publish(new StringEvent(EventType.PRINT_RECEIPT,
+		// resultPrice));
+		// System.out.println("Pay successfully!");
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Enter the card number: ");
 		String cardNumber = scanner.nextLine();
 
-		System.out.print("Eneter the card expiration date 'mm/yy': ");
-		String date = scanner.nextLine();
+		System.out.print("Enter the card expiration date 'mm/yy': ");
+		String expiredDate = scanner.nextLine();
 
-		System.out.print("Enter the CVV code:");
-		String CVV = scanner.nextLine();
+		System.out.print("Enter the CVV code: ");
+		String cvv = scanner.nextLine();
 
-		if (!(cardNumber.matches("\\d{16}") && date.matches("0[1-9]|1[0-2]/\\d{2}")
-				&& CVV.matches("\\d{3}"))) {
-			System.out.print("Pay failed.\n");
-			return;
+		if (cardNumber.matches("\\d{16}") && expiredDate.matches("((0[1-9])|(1[0-2]))/\\d{2}")
+				&& cvv.matches("\\d{3}")) {
+			DecimalFormat format = new DecimalFormat("###.00");
+			String resultPrice = "$" + format.format((totalPrice * 0.9));
+
+			EventManager.getInstance().publish(new StringEvent(EventType.PRINT_RECEIPT, resultPrice));
+
+			System.out.println("Pay successfully!");
+		} else {
+			System.out.println("Pay fail");
 		}
-
-		DecimalFormat format = new DecimalFormat("###.00");
-		String resultPrice = "$" + format.format((totalPrice - (totalPrice * 0.1)));
-		EventManager.getInstance().publish(new StringEvent(EventType.PRINT_RECEIPT, resultPrice));
-		System.out.println("Pay successfully!");
-
-		// Scanner scanner = new Scanner(System.in);
-
-		// System.out.print("Enter the card number: ");
-		// String card = scanner.nextLine();
-		// System.out.print("Enter the card expiration date 'mm/yy': ");
-		// String expiredDate = scanner.nextLine();
-		// System.out.print("Enter the CVV code: ");
-		// String cvv = scanner.nextLine();
-
-		// if (card.matches("\\d{16}") && expiredDate.matches("((0[1-9])|(1[0-2]))/\\d{2}") && cvv.matches("\\d{3}")) {
-		// 	DecimalFormat f = new DecimalFormat("###.00");
-		// 	String data = "$" + f.format((totalPrice - (totalPrice * 0.1)));
-
-		// 	EventManager.getInstance().publish(new StringEvent(EventType.PRINT_RECEIPT, data));
-
-		// 	System.out.println("Pay successfully!");
-		// } else {
-		// 	System.out.println("Pay fail");
-		// }
 	}
 }
